0158755987508040cb984b650fbe9a76f5519e14
Richard <Richard@128.189.210.249>
2012-02-08 15:17:42 -0800

    -constructor and method added -print "Richard" added -documentation added

--- test/cpsc310/TestClass.java
+++ test/cpsc310/TestClass.java
@@ -2,4 +2,22 @@ package cpsc310;
 
 public class TestClass {
 
+	/**
+	 * Emtpy constructor for TestClass Class
+	 */
+	public TestClass()
+	{
+		
+	}
+	
+	/**
+	 * Method to print out the names of all team members.
+	 * 
+	 * Note: Just add a print statement to this function.
+	 */
+	public void printNames()
+	{
+		System.out.println("Richard");
+	}
+	
 }
