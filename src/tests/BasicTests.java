7d6c2a6d3746fb3d2ad572cef096a656e53d6a16
Richard <Richard@192.168.1.104>
2012-02-28 23:19:21 -0800

    Added "tests" package and transferred JUnit tests from lab exercises to serve as templates for our JUnit tests.  (Some reason JUnit 4 isn't an option to commit so hopefully it will be added soon).

---
+++ src/tests/BasicTests.java
@@ -0,0 +1,65 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+//import calc.Calculator;
+
+public class BasicTests {
+/*
+	private static Calculator calculator = new Calculator();
+
+	@Before
+	public void setUp() {
+		calculator.reset();
+	}
+
+	@Test
+	public void testAdd() {
+		calculator.add(1);
+		calculator.add(1);
+		assertEquals(calculator.getResult(), 2);
+	}
+
+	@Test
+	public void testSubtract() {
+		calculator.add(10);
+		calculator.substract(2);
+		assertEquals(calculator.getResult(), 8);
+	}
+
+	@Test
+	public void testDivide() {
+		calculator.add(8);
+		calculator.divide(2);
+		//changed assert to assertEquals()
+		assertEquals(calculator.getResult(), 5);
+	}
+	
+	@Test
+	public void testReset(){
+		calculator.reset();
+		assertTrue(calculator.getResult()==0);
+	}
+
+	//commented out the ignore
+	//@Ignore("not implemented yet")
+	@Test
+	public void testMultiply() {
+		calculator.add(10);
+		calculator.multiply(10);
+		assertEquals(calculator.getResult(), 100);
+	}
+	
+	@After
+	public void cleanUp()
+	{
+		// Do nothing.
+	}
+*/
+}
