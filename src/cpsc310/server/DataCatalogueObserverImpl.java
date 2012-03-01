573dbc3538f74e859a6432244aa31e21fc027ff2
Richard <Richard@192.168.1.104>
2012-02-29 20:58:38 -0800

    Removed unnecessary imports and added a "TO DO" comment for Sprint 2.

--- src/cpsc310/server/DataCatalogueObserverImpl.java
+++ src/cpsc310/server/DataCatalogueObserverImpl.java
@@ -4,8 +4,6 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import cpsc310.client.DataCatalogueObserver;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -13,15 +11,6 @@ import java.net.URLConnection;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.SingleClientConnManager;
-import org.apache.http.params.BasicHttpParams;
 
 @SuppressWarnings("serial")
 public class DataCatalogueObserverImpl extends RemoteServiceServlet implements DataCatalogueObserver
@@ -46,6 +35,7 @@ public class DataCatalogueObserverImpl extends RemoteServiceServlet implements D
 			URL fileLocation = new URL(urlLink);
 			//open connection to the file
 			URLConnection fileConnection = fileLocation.openConnection();
+			//TO DO: Implement checking to see if latest version of file is already possessed.		
 			//set the reading timeout before retrieving and reading from the input stream
 			fileConnection.setReadTimeout(timeOut);
 			//retrieve the input stream and store each line from the file into a list
