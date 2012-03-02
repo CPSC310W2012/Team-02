c4ee22a15b241ca52627e9c43b4e53416076d8cc
Richard <Richard@192.168.1.104>
2012-03-01 23:46:25 -0800

    Updated FileParser with comments/documentation.  Updated test cases for the Observer class.  Created JUnit tests for the FileParser.  Created Exception class (DownloadFailedException) for Observer class for Sprint 2.

---
+++ src/exceptions/DownloadFailedException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+/**
+ * Generic exception for file download problems and errors.
+ */
+@SuppressWarnings("serial")
+public class DownloadFailedException extends Exception {
+
+	public DownloadFailedException(String string) {
+		//TO DO: Sprint 2 Exception for Download
+	}
+
+}
