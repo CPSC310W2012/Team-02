33e7918364bd199b80b820532d9a0def04bac27a
Richard <Richard@128.189.225.187>
2012-03-09 12:17:36 -0800

    Added file stub for writing integration tests.

---
+++ src/tests/IntegrationTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntegrationTests {
+
+	//test file for writing tests to ensure that different classes work correctly upon
+	//integration
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
