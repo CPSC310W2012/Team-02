f5febccdef8eb5a1a174bce7f6f0d439841bd1e4
Justin Chu <justinchu1989@gmail.com>
2012-03-23 06:44:32 -0700

    Performance Tests

---
+++ src/tests/HouseDataServiceTests.java
@@ -0,0 +1,67 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import cpsc310.server.DataStore;
+import cpsc310.server.HouseDataServiceImpl;
+
+public class HouseDataServiceTests {
+
+	// tests to ensure that the DataStore works correctly
+
+	public static HouseDataServiceImpl service;
+
+	// Instantiate
+	@BeforeClass
+	public static void setUp() throws Exception {
+		service = new HouseDataServiceImpl();
+	}
+
+	// test performance of Search, worst case
+	@Test
+	public void testSearchPerformWorst() {
+		// [0]"civicNumber"
+		// [1]"StreetName",
+		// Value "Current Land Value" - [2]min, [3]max
+		// Value "Price" - [4]min, [5]max
+		// [6]"Realtor"
+		// [7]"Postal Code"
+		// Value "Current Improvement Value" - [8]min, [9]max
+		// Year "Assessment Year" - [10]min, [11]max
+		// Value "Previous Land Value" - [12]min, [13]max
+		// Value "Previous Improvement Value" - [14]min, [15]max
+		// Year "Year Built" - [16]min, [17]max
+		// Year "Big Improvement Year" - [18]min, [19]max
+		String[] userSearchInput = { "", "", "0", "999999999", "0",
+				"999999999", "", "", "0", "999999999", "0", "999999999", "0",
+				"999999999", "0", "999999999", "0", "999999999", "0",
+				"999999999" };
+		service.searchHouses(userSearchInput, -1);
+	}
+
+	// test performance of Search, best case
+	@Test
+	public void testSearchPerformBest() {
+		// [0]"civicNumber"
+		// [1]"StreetName",
+		// Value "Current Land Value" - [2]min, [3]max
+		// Value "Price" - [4]min, [5]max
+		// [6]"Realtor"
+		// [7]"Postal Code"
+		// Value "Current Improvement Value" - [8]min, [9]max
+		// Year "Assessment Year" - [10]min, [11]max
+		// Value "Previous Land Value" - [12]min, [13]max
+		// Value "Previous Improvement Value" - [14]min, [15]max
+		// Year "Year Built" - [16]min, [17]max
+		// Year "Big Improvement Year" - [18]min, [19]max
+		String[] userSearchInput = { "4203", "13TH AVE W", "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "", "", "", "", "", "", "" };
+		service.searchHouses(userSearchInput, -1);
+	}
+
+}
